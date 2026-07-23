7921c53a6a2fe5a1493cc4ec56d60628c8e42e04
Bozhidar Batsov <bozhidar@batsov.dev>
2026-07-23 16:44:20 +0200

    Add easy-kill face support
    The kill candidate uses the region color (it's a selection in all
    but name) and the origin point gets a red marker.

--- test/tokyo-night-test.el
+++ test/tokyo-night-test.el
@@ -188,7 +188,8 @@ frame-side face recomputation (which is unreliable in batch)."
            nrepl-message-1-face nrepl-message-8-face)
     (vundo vundo-node vundo-stem vundo-branch-stem vundo-highlight
            vundo-saved vundo-last-saved vundo-diff-highlight)
-    (volatile-highlights vhl/default-face))
+    (volatile-highlights vhl/default-face)
+    (easy-kill easy-kill-selection easy-kill-origin))
   "Alist of (PACKAGE . FACES) the theme is expected to cover.")
 
 (describe "package face coverage"
